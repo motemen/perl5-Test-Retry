0768d052891579e477acebe24d223bce2b3ec5ae
motemen <motemen@gmail.com>
2013-02-14 15:45:28 +0900

    Initial commit

---
+++ lib/Test/Retry.pm
@@ -0,0 +1,33 @@
+package Test::Retry;
+use strict;
+use warnings;
+use 5.008_001;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Test::Retry - 
+
+=head1 SYNOPSIS
+
+  use Test::Retry;
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
